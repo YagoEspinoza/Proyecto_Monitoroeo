83fe6384ff866371810fc95292912cd674519c4d
Yago Espinoza <74250016@continental.edu.pe>
2026-06-29 12:27:33 -0500

    Resolver errores de mi proyecto

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,8 +2,4 @@ export const environment = {
   production: true,
   apiUrl: 'https://netguard-backend-yapa.onrender.com/api',
   wsUrl: 'https://netguard-backend-yapa.onrender.com'
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 157138c09220bbbe873320bb75947fdda592e96c
